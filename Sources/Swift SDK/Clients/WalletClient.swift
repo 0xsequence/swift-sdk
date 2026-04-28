33aaf0fe97c82d94ec255439600e4064af1ce9bb
Andy Grüning <andygruening@gmail.com>
2026-04-28 14:18:33 +0200

    made indexer vars public

--- Sources/Swift SDK/Clients/WalletClient.swift
+++ Sources/Swift SDK/Clients/WalletClient.swift
@@ -11,7 +11,7 @@ public class WalletClient {
     var verifier = "";
     var challenge = "";
     
-    public init(projectAccessKey: String, environment: OmsEnvironment = OmsEnvironment()) {
+    public init(projectAccessKey: String, environment: OMSClientEnvironment = OMSClientEnvironment()) {
         if let walletAddress = try? keychain.string(forKey: Constants.addressStorageKey),
            let walletId = try? keychain.string(forKey: Constants.walletIdStorageKey),
            let signerPrivateKeyHex = try? keychain.string(forKey: Constants.signerStorageKey) {
